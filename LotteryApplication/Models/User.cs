e84b0921d84cea518df092d65fbb83a74673b914
Amine Jaoui <aminejaoui01@gmail.com>
2022-12-17 22:25:44 +0100

    Add project files.

---
+++ LotteryApplication/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace LotteryApplication.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; }
+    }
+    
+}
