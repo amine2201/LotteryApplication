88e6e3351459c594aca498347b57bd7a9cbc34fd
Amine Jaoui <aminejaoui01@gmail.com>
2022-12-18 20:11:27 +0100

    IdentityUser changed to ApplicationUser

--- LotteryApplication/DBContext/ApplicationDbContext.cs
+++ LotteryApplication/DBContext/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using LotteryApplication.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
--- LotteryApplication/Program.cs
+++ LotteryApplication/Program.cs
@@ -2,7 +2,6 @@ using LotteryApplication.DBContext;
 using LotteryApplication.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using IdentityRole = LotteryApplication.Models.IdentityRole;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +10,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
     .AddDefaultTokenProviders();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -30,9 +32,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
