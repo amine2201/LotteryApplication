4d2539782e1f2e1338398ee1dbbde572a8c1f8cb
Amine Jaoui <aminejaoui01@gmail.com>
2022-12-20 19:02:32 +0100

    Creating Admin Dashboard

---
+++ LotteryApplication/Controllers/ParticipationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LotteryApplication.DBContext;
+using LotteryApplication.Models;
+
+namespace LotteryApplication.Controllers
+{
+    public class ParticipationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ParticipationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+    }
+}
