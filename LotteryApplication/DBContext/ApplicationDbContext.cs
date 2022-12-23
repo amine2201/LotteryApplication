fb96124d7c575d69563982aa2dea770f367ded0a
Amine Jaoui <aminejaoui01@gmail.com>
2022-12-23 17:07:41 +0100

    again

--- LotteryApplication/DBContext/ApplicationDbContext.cs
+++ LotteryApplication/DBContext/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Runtime.Intrinsics.X86;
 
 namespace LotteryApplication.DBContext
 {
@@ -10,22 +9,15 @@ namespace LotteryApplication.DBContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            To create a one-to - one relationship between the ApplicationUser and Participation entities, you can use Entity Framework's fluent API in the OnModelCreating method of your Entity Framework DbContext class. Here is an example of how you can do this:
+        
 
-Copy code
-public class MyDbContext : DbContext
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-            public DbSet<Participation> Participations { get; set; }
-
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                modelBuilder.Entity<Participation>()
-                    .HasOne(p => p.Participant)
-                    .WithOne(a => a.Participation)
-                    .HasForeignKey<Participation>(p => p.ParticipantId);
-            }
-
+            modelBuilder.Entity<Participation>()
+                .HasOne(p => p.Participant)
+                .WithOne(a => a.Participation)
+                .HasForeignKey<Participation>(p => p.ParticipantId);
         }
         public DbSet<ApplicationUser> applicationUsers { get; set; }
         public DbSet<IdentityRole> roleUsers { get; set; }
